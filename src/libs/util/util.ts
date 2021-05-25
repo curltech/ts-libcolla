79995ede98bc6f9f013d7377e3ed34bfc41fd3fa
Jinhe Zhao <goldjinhe@126.com>
2021-05-25 15:48:55 +0800

    asyncPool

--- src/libs/util/util.ts
+++ src/libs/util/util.ts
@@ -525,4 +525,30 @@ export class CollaUtil {
 		}
 		return from
 	}
+	static asyncPool(poolLimit, array, iteratorFn) {
+		let i = 0;
+		const ret = [];// 用来存储所有的Promise任务
+		const executing = []; // 正在执行的Promise对象
+		
+		const enqueue = function () {
+			// 边界处理，array为空数组
+			if (i === array.length) {
+				return Promise.resolve();
+			}
+			const item = array[i++];  // 获取参数
+			const p = Promise.resolve().then(() =>{
+			  iteratorFn(item, array)
+			}); // 初始化一个promise
+			ret.push(p); // 放入promises数组
+			// promise执行完毕，从executing数组中删除
+			const e = p.then(() => executing.splice(executing.indexOf(e), 1));
+			// 插入executing数字，表示正在执行的promise
+			executing.push(e);
+			// 使用Promise.rece，每当executing数组中promise数量低于poolLimit，就实例化新的promise并执行
+			let r =  executing.length >= poolLimit?Promise.race(executing):Promise.resolve()
+			// 递归，直到遍历完array
+			return r.then(() => enqueue());
+		};
+		return enqueue().then(() => Promise.allSettled(ret)); // 最后呢，用Promise.all 等待全部任务完成，返回Promise的结果输出
+	}
 }
